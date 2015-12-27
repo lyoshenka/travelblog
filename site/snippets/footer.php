d83ef865981cf9919102e78d3f36c021d005c3f9
Alex Grintsvayg <lyoshenka@gmail.com>
2015-12-27 17:47:50 -0500

    correct analytics code

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -17,7 +17,7 @@
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-48451015-2', 'auto');
+    ga('create', 'UA-48451015-5', 'auto');
 
     InstantClick.on('change', function() {
       ga('send', 'pageview', location.pathname + location.search);
